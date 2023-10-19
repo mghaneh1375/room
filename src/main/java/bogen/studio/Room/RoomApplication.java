7b9c64be5d8cb7d5e9dd2ebf9930c8aa28ec4a7d
mghaneh1375 <mghaneh1375@gmail.com>
2023-10-19 12:02:49 +0330

    nothing

--- src/main/java/bogen/studio/Room/RoomApplication.java
+++ src/main/java/bogen/studio/Room/RoomApplication.java
@@ -2,29 +2,15 @@ package bogen.studio.Room;
 
 import bogen.studio.Room.Controller.JobHandler;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import java.util.TimeZone;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@OpenAPIDefinition(info = @Info(title = "Koochita Boom API", version = "2.0", description = "Koochita Information"))
+@SpringBootApplication
+//@OpenAPIDefinition(info = @Info(title = "Koochita Boom API", version = "2.0", description = "Koochita Information"))
 @EnableMongoAuditing
-@ComponentScan({"bogen.studio.Room.*"})
-@EntityScan("bogen.studio.Room.*")
-@Configuration
 public class RoomApplication {
 
     public static void main(String[] args) {
@@ -33,20 +19,6 @@ public class RoomApplication {
         ApplicationContext applicationContext = SpringApplication.run(RoomApplication.class, args);
         JobHandler jobHandler = applicationContext.getBean(JobHandler.class);
         jobHandler.run();
-    }
-
-    private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                .bearerFormat("JWT")
-                .scheme("bearer");
-    }
-
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().
-                        addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes
-                        ("Bearer Authentication", createAPIKeyScheme()));
     }
 
 }
--- src/main/java/bogen/studio/Room/Security/SecurityConfig.java
+++ src/main/java/bogen/studio/Room/Security/SecurityConfig.java
@@ -59,4 +59,18 @@ public class SecurityConfig {
 
     }
 
+
+//    private SecurityScheme createAPIKeyScheme() {
+//        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+//                .bearerFormat("JWT")
+//                .scheme("bearer");
+//    }
+//
+//    @Bean
+//    public OpenAPI openAPI() {
+//        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+//                        addList("Bearer Authentication"))
+//                .components(new Components().addSecuritySchemes
+//                        ("Bearer Authentication", createAPIKeyScheme()));
+//    }
 }
